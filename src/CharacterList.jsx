a72d5fc9f99522898b76656534f8ef98f09ece90
Kenny Jarnagin <jarnaginkenny@gmail.com>
2022-07-20 00:19:57 -0400

    Resolve eslint issues

--- src/CharacterList.jsx
+++ src/CharacterList.jsx
@@ -3,18 +3,16 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Character from './Character';
 
 function CharacterList() {
-  // eslint-disable-next-line no-unused-vars
   const [characters, setCharacters] = useState([]);
 
   const fetchCharacters = useCallback(async () => {
-    // eslint-disable-next-line
-     const res = await fetch('https://rickandmortyapi.com/api/character');
+    const res = await fetch('https://rickandmortyapi.com/api/character');
     const characterList = await res.json();
     setCharacters(characterList.results);
   }, []);
 
   useEffect(() => {
-    fetchCharacters().catch(console.error);
+    fetchCharacters();
   }, [fetchCharacters]);
 
   return (
